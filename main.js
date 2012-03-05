58599f25d7b184e8140b51167c9f9b65de7f1dba
Kat Lau <katinthehat131@gmail.com>
2012-03-04 23:42:41 -0500

    first commit

--- main.js
+++ main.js
@@ -1,116 +1,32 @@
+//set load hooks
 $(document).ready(function(){
-    
-	$('#logo').click(showHome);
-    //what happens when you click the new crush button?
-    $('#aboutLink').click(showAbout);
-    $('#membersLink').click(showMembers);
-    $('#musicLink').click(showMusic);
-    $('#auditionLink').click(showAudition);
-	$('#showsLink').click(showShows);
-    $('#contactLink').click(showContact);
-    
+    //what happens when we click a 'nav' link:
+    $('.nav').click(function(e){   
+        //stop normal browser link action
+        //i forget, if you have a glitch you need to add something here like:
+        //e.stopDefaultHandler?  
+        //get panel name from link href
+        var pLink = $(this).attr('href').replace('#', '');
+        //change to that panel
+        changePanel( pLink );      
+    });  
 });
 
-function showLink(){
-	$(this).slideUp('slow', function () {
-		//Animation complete.
-	});
-	[link clicked] = [div name to show]
-	[hide all other divs]/[slide up whatever div is currently displayed]
-	[show "this" div] with slideup animation
-	
-}
-
-
-
-//Our show functions
-function showHome(){
-   $('#home').slideUp('fast', function() {
-	    // Animation complete.
-	  });	
-   $('#home').show();
-   $('#about').hide();
-   $('#members').hide();
-   $('#music').hide();
-   $('#audition').hide();
-   $('#shows').hide();
-   $('#contact').hide();
-}
-
-function showAbout(){
-	$('#about').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').show();
-   $('#members').hide();
-   $('#music').hide();
-   $('#audition').hide();
-   $('#shows').hide();
-   $('#contact').hide();
-}
-
-function showMembers(){
-	$('#members').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').hide();
-   $('#members').show();
-   $('#music').hide();
-   $('#audition').hide();
-   $('#shows').hide();
-   $('#contact').hide();
-}
-
-function showMusic(){
-	$('#music').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').hide();
-   $('#members').hide();
-   $('#music').show();
-   $('#audition').hide();
-   $('#shows').hide();
-   $('#contact').hide();
-}
-
-function showAudition(){
-	$('#audition').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').hide();
-   $('#members').hide();
-   $('#music').hide();
-   $('#audition').show();
-   $('#shows').hide();
-   $('#contact').hide();
-}
-
-function showShows(){
-	$('#shows').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').hide();
-   $('#members').hide();
-   $('#music').hide();
-   $('#audition').hide();
-   $('#shows').show();
-   $('#contact').hide();
-}
-
-function showContact(){
-	$('#contact').slideUp('fast', function() {
-	    // Animation complete.
-	  });
-   $('#home').hide();
-   $('#about').hide();
-   $('#members').hide();
-   $('#music').hide();
-   $('#audition').hide();
-   $('#shows').hide();
-   $('#contact').show();
+function changePanel( pname ){
+    //all our panels
+    var pages = ['about', 'members', 'music', 'audition', 'shows', 'contact'];
+    
+    //the height of any panel
+    var pheight = $('.panel').first().outerHeight();
+ 
+    //the index of the panel we want
+    var i = pages.indexOf( pname );
+    var target = -1 * i * pheight;
+    
+    //the offset to view that panel    
+    target = String( target ) + 'px';
+    
+    //slide to that panel offset
+    $('.panel').first().animate({'marginTop': target});
+    
 }
